6d1325c0447298a1cfa4b093094e7949a6b31699
squest <sabdaps@gmail.com>
2016-07-04 06:42:24 +0700

    fixed the isort

--- Racket/sorting-test.rkt
+++ Racket/sorting-test.rkt
@@ -5,15 +5,13 @@
 (test-case
  "Testing quicksort"
 
- (display "Entering sorting tests\n")
-
  (display "Testing quicksort\n")
 
- (check-equal? (range 20)
-	       (quick-sort (shuffle (range 20))))
+ (check-equal? (quick-sort (shuffle (range 20)))
+	       (range 20))
  
  (let [(lst (range 1 10 0.2))]
-   (check-equal? lst (quick-sort (shuffle lst))))
+   (check-equal? (quick-sort (shuffle lst)) lst))
  
  (check-equal? (quick-sort '()) '())
 
@@ -21,6 +19,46 @@
 
  (let* [(lst (range 5000))
 	(slst (shuffle lst))]
-   (check-equal? lst (time (quick-sort slst)))))
+   (check-equal? (time (quick-sort slst)) lst)))
+
+(test-case
+ "Testing mergesort"
+
+ (display "Testing mergesort\n")
+
+ (check-equal? (merge-sort (shuffle (range 20)))
+	       (range 20))
+ 
+ (let [(lst (range 1 10 0.2))]
+   (check-equal? (merge-sort (shuffle lst)) lst))
+ 
+ (check-equal? (merge-sort '()) '())
+
+ (display "Time taken for 5000 elements sorting\n")
+
+ (let* [(lst (range 5000))
+	(slst (shuffle lst))]
+   (check-equal? (time (merge-sort slst)) lst)))
+
+(test-case
+ "Testing insertion sort"
+
+ (display "Testing insertion sort\n")
+
+ (check-equal? (isort (shuffle (range 20)))
+	       (range 20))
+ 
+ (let [(lst (range 1 10 0.2))]
+   (check-equal? (isort (shuffle lst)) lst))
+ 
+ (check-equal? (isort '()) '())
+
+ (display "Time taken for 5000 elements sorting\n")
+
+ (let* [(lst (range 5000))
+	(slst (shuffle lst))]
+   (check-equal? (time (isort slst)) lst)))
+
+
 
 
