268ee69d27af4ecca89ca105ed681b1f9b297529
squest <sabdaps@gmail.com>
2016-07-05 15:27:55 +0700

    try travis

--- rosetta/project.clj
+++ rosetta/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :min-lein-version "2.5.3"
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]
   :source-paths ["src/clj"])
